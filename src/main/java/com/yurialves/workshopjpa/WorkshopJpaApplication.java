00068c4b0207fbfa761327687570ad9651dc1570
Yuri Alves <yuri.o.alves2@outlook.com>
2022-02-08 19:55:44 -0300

    Project Created

---
+++ src/main/java/com/yurialves/workshopjpa/WorkshopJpaApplication.java
@@ -0,0 +1,13 @@
+package com.yurialves.workshopjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurialves/workshopjpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurialves.workshopjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
