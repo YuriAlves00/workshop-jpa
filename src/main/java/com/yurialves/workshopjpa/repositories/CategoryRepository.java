289ec08d520ee8f819d83de4c1bdbd1fdca85910
Yuri Alves <yuri.o.alves2@outlook.com>
2022-02-12 23:35:39 -0300

    Category

---
+++ src/main/java/com/yurialves/workshopjpa/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.yurialves.workshopjpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yurialves.workshopjpa.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	
+	
+
+}
